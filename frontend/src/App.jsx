7ce1916489fffd32428e8cb998a75e87c842393e
Coding2alpha <omsinghthakur2502@gmail.com>
2024-03-03 21:27:01 +0530

    Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import AddProduct from "./pages/AddProduct.jsx";
 
 function App() {
-
-  return <div className="bg-slate-300"><Header /></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="pt-16">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/add" element={<AddProduct />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,59 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaRegUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <div>Header</div>
-  )
-}
-export default Header
+    <div className="fixed flex h-16 pt-2 shadow-md w-full md:justify-between items-center px-1 md:px-4 z-10">
+      <Link to={"/"}><div>
+        <img className="w-25 h-14 items-center pb-1" src={logo} alt="" />
+      </div></Link>
+      <nav className="flex md:justify-between items-center">
+        <Link to={"/home"} className="px-1 md:px-2 text-xl">
+          Home
+        </Link>
+        <Link to={"/menu"} className="px-1 md:px-2 text-xl">
+          Menu
+        </Link>
+        <Link to={"/about"} className=" px-1 md:px-2 text-xl">
+          About
+        </Link>
+        <Link to={"/contact"} className="px-1 md:px-2 text-xl">
+          Contact
+        </Link>
+      </nav>
+      <div className="flex items-center">
+        <div className="text-2xl md:text-5xl md:mr-6 relative">
+          <Link to={"/cart"}>
+            <FaCartShopping />
+            <div className="absolute -top-1 -right-1 text-white bg-red-500 md:h-6 md:w-6 h-4 w-4 rounded-full m-0 p-0 text-sm text-center ">
+              0
+            </div>
+          </Link>
+        </div>
+        <div className="">
+          <div className="ext-2xl md:text-5xl md:px-4">
+            <FaRegUserCircle onClick={() => setShowMenu(!showMenu)} />
+          </div>
+          {showMenu && (
+            <div className="absolute shadow-md p-2 right-2 ">
+              <Link to={"/add"}>
+                <div className="py-1 text-xl whitespace-nowrap">
+                  Add Product
+                </div>
+              </Link>
+              <Link to={"/register"}>
+                <div className="text-xl whitespace-nowrap">Register</div>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
