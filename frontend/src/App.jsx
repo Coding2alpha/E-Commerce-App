fc25685b7a27c140a304d492c673d3004b9f8d73
Coding2alpha <omsinghthakur2502@gmail.com>
2024-03-03 19:18:43 +0530

    setup tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+
+  return <div className="bg-slate-300"><Header /></div>;
+}
+
+export default App;
