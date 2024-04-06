95a3454953120152c2ccecee859a9075efe3da6e
Coding2alpha <omsinghthakur2502@gmail.com>
2024-04-07 03:44:29 +0530

    app.js changes

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,13 @@ const cartRouter = require("./routes/cart");
 const authUser = require("./middleware/authentication");
 
 const app = express();
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://ufit-eshop.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(
+  cors({
+    origin: "https://ufit-eshop.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use("", authRouter);
 app.use("/product", authAdmin, productRouter);
